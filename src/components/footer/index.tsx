68a7bc77287ff5b648dfd592bce6358533bcce1f
Fernando Guerreiro <f.luhis@gmail.com>
2025-01-17 18:59:38 -0300

    fix: ajusta layout do rodapé para melhor responsividade e alinhamento

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
     <footer className=" bg-gray-800 text-white fixed bottom-0 w-full z-50 shadow-md">
-      <div className="flex flex-col items-center py-2">
-        <p className="text-sm mt-2">
+      <div className="flex flex-col lg:flex-row justify-around py-2 items-center flex-wrap gap-x-2 gap-y-1">
+        <p className="text-sm">
           Contato:{" "}
           <a
             href="mailto:dev.fernandoguerreiro@gmail.com"
@@ -12,7 +12,7 @@ export function Footer() {
             dev.fernandoguerreiro@gmail.com
           </a>
         </p>
-        <p className="text-sm mt-2">
+        <p className="text-sm">
           Telefone/WhatsApp:{" "}
           <a
             href="http://api.whatsapp.com/send?phone=5588999254660"
@@ -22,7 +22,7 @@ export function Footer() {
             (88) 99925-4660
           </a>
         </p>
-        <p className="text-xs mt-2">
+        <p className="text-xs">
           © 2025 Fernando Guerreiro. Todos os direitos reservados.
         </p>
       </div>
