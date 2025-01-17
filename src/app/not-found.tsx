4d450853d9b04628790b331b18fe9d4afce9898c
Fernando Guerreiro <f.luhis@gmail.com>
2025-01-17 19:35:44 -0300

    feat: adiciona componente de página 404 para tratamento de erros de navegação

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen flex flex-col items-center justify-center px-2 pt-16 pb-[116px]">
+      <h1 className="text-white text-xl md:text-4xl font-bold">
+        404 - Página não encontrada
+      </h1>
+    </div>
+  );
+}
