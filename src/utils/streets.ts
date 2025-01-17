a2471c5650d78d8915249198a92d5d288ad8880e
Fernando Guerreiro <f.luhis@gmail.com>
2025-01-17 19:03:00 -0300

    refactor: remove abreviações desnecessárias de nomes de ruas

--- src/utils/streets.ts
+++ src/utils/streets.ts
@@ -1,8 +1,5 @@
 export const streets = [
   { name: "Rosa Teresa de Jesus" },
-  { name: "Rua Rosa Teresa de Jesus" },
-  { name: "Rosa Teresa" },
-  { name: "Rua Dom Pedro i" },
   { name: "Dom Pedro i" },
-  { name: "Avenida Coronel Francisco Remígio" },
+  { name: "Coronel Francisco Remígio" },
 ];
