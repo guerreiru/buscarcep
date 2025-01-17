827af0d449ff784c30a9cfc7adf533771dd5592a
Fernando Guerreiro <fernando.guerreiro@dsm.com>
2025-01-17 14:09:31 -0300

    fix: ajusta padding da seção sobre para melhor responsividade

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen flex flex-col items-center pb-2 py-4 pt-16">
+    <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen flex flex-col items-center px-1 pt-16 pb-[116px]">
       <main className="w-full max-w-3xl text-center">
         <h2 className="text-3xl font-bold text-blue-300 mb-4">
           Sobre o Projeto
