a9c76cf4d922c5c8a18076742ca33cbc08b907a4
Fernando Guerreiro <fernando.guerreiro@dsm.com>
2025-01-27 11:18:46 -0300

    feat: adiciona funcionalidade de copiar código Pix e exibe QR code no footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { Modal } from "../modal";
+import Image from "next/image";
+import { PIX_COPIA_E_COLA } from "@/utils/constants";
+
 export function Footer() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const hiddenInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleCopyPixCode = async () => {
+    try {
+      if (navigator.clipboard && navigator.clipboard.writeText) {
+        await navigator.clipboard.writeText(PIX_COPIA_E_COLA);
+      } else {
+        if (hiddenInputRef.current) {
+          hiddenInputRef.current.value = PIX_COPIA_E_COLA;
+          hiddenInputRef.current.select();
+          document.execCommand("copy");
+        }
+      }
+      alert("Código copiado com sucesso!");
+    } catch (err) {
+      alert("Erro ao copiar o código.");
+    }
+  };
+
   return (
     <footer className=" bg-gray-800 text-white fixed bottom-0 w-full z-50 shadow-md">
-      <div className="flex flex-col lg:flex-row justify-around py-2 items-center flex-wrap gap-x-2 gap-y-1">
+      <div className="flex flex-col lg:flex-row justify-around py-2 items-center flex-wrap gap-2">
         <p className="text-sm">
           Contato:{" "}
           <a
@@ -22,10 +49,55 @@ export function Footer() {
             (88) 99925-4660
           </a>
         </p>
+        <p className="text-sm text-center">
+          Gostou do serviço? Apoie a manutenção do site ❤️!{" "}
+          <span
+            className="text-gray-200 hover:text-blue-300 hover:underline"
+            aria-label="Ligar ou mandar uma mensagem no WhatsApp 88 99925 4660"
+            onClick={() => setModalOpen(true)}
+          >
+            Clique aqui para ver o qr code
+          </span>
+        </p>
         <p className="text-xs">
           © 2025 Fernando Guerreiro. Todos os direitos reservados.
         </p>
       </div>
+
+      {modalOpen && (
+        <Modal onClose={() => setModalOpen(false)}>
+          <input
+            ref={hiddenInputRef}
+            type="hidden"
+            readOnly
+            style={{ display: "none", visibility: "hidden" }}
+          />
+
+          <div className="flex flex-col items-center justify-center gap-1">
+            <Image
+              alt="Qr code para a chave pix dev.fernandoguerreiro@gmail.com"
+              src="/qr-code.png"
+              width={200}
+              height={200}
+            />
+
+            <button
+              className="border border-green-700 py-2 px-4 rounded-sm text-gray-800 shadow-sm hover:bg-slate-100 hover:border-green-800"
+              onClick={handleCopyPixCode}
+            >
+              Copiar o código do pix
+            </button>
+
+            <p className="text-sm text-gray-800">
+              Nome: Fernando Luiz Guerreiro Rodrigues
+            </p>
+            <p className="text-sm text-gray-800">CPF: ***.881.983-**</p>
+            <p className="text-sm text-gray-800">
+              Instituição: Bco Itaucard S.A.
+            </p>
+          </div>
+        </Modal>
+      )}
     </footer>
   );
 }
