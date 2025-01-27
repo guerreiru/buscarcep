83c94b318c46fd09f45c50f9c434926c52b6856d
Fernando Guerreiro <fernando.guerreiro@dsm.com>
2025-01-27 11:32:19 -0300

    feat: ajusta espaçamento inferior das páginas About e Home para melhor layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen flex flex-col items-center px-2 pt-16 pb-[116px]">
+    <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen flex flex-col items-center px-2 pt-16 pb-[164px]">
       <main className="w-full max-w-3xl">
         <h2 className="text-3xl font-bold text-blue-300 mb-4">
           Sobre o Projeto
