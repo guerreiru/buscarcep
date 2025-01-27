9a2489b34598bd20e0dc56cda80a7f691d34d0f8
Fernando Guerreiro <fernando.guerreiro@dsm.com>
2025-01-27 09:43:13 -0300

    feat: adiciona script do Google AdSense ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,11 @@ export default function RootLayout({
             }),
           }}
         ></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8976093869692251"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
