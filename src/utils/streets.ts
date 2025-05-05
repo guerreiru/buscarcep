7ecf2c867533fe5ddf669f1e5c59bb1e6eef37b2
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-05-05 20:49:54 -0300

    feat: adiciona novas ruas à lista de ruas disponíveis

--- src/utils/streets.ts
+++ src/utils/streets.ts
@@ -2,4 +2,6 @@ export const streets = [
   { name: "Rosa Teresa de Jesus" },
   { name: "Dom Pedro i" },
   { name: "Coronel Francisco Remígio" },
+  { name: "Cremilde Osterne" },
+  { name: "Cremilde" },
 ];
