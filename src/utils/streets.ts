8a91b6dc825e40b93c616a2f5b7b5d5621f3b2ba
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-05-08 12:52:22 -0300

    feat: remove redundant street entries for consistency

--- src/utils/streets.ts
+++ src/utils/streets.ts
@@ -1,8 +1,6 @@
 export const streets = [
   { name: "Rosa Teresa de Jesus" },
   { name: "Dom Pedro i" },
-  { name: "Coronel Francisco Remígio" },
-  { name: "Cremilde Osterne" },
   { name: "Cremilde" },
   { name: "Fidelis" },
 ];
